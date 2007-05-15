64dd2db5ece4437e8d685831637aac6692e8c9db
blakharaz <blakharaz@fd4443c4-ef63-4c3a-894f-489cd2888b9e>
2007-05-15 21:09:54 +0000

    Provide linux build for meshmagick
    git-svn-id: https://svn.code.sf.net/p/ogreaddons/code/trunk/meshmagick@2221 fd4443c4-ef63-4c3a-894f-489cd2888b9e

--- src/MeshMergeTool.cpp
+++ src/MeshMergeTool.cpp
@@ -19,6 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "MeshMergeTool.h"
 
+#include <stdexcept>
 #include <OgreMeshManager.h>
 #include <OgreHardwareBufferManager.h>
 #include <OgreSubMesh.h>
