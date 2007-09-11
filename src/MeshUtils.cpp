9a29918a859d94dab0ae325c5583b463c1fca6b7
blakharaz <blakharaz@fd4443c4-ef63-4c3a-894f-489cd2888b9e>
2007-09-11 23:30:00 +0000

    Fix case of OgreSubMesh.h #include
    git-svn-id: https://svn.code.sf.net/p/ogreaddons/code/trunk/meshmagick@2316 fd4443c4-ef63-4c3a-894f-489cd2888b9e

--- src/MeshUtils.cpp
+++ src/MeshUtils.cpp
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 #include "MeshUtils.h"
 
-#include <OgreSubmesh.h>
+#include <OgreSubMesh.h>
 
 using namespace Ogre;
 
